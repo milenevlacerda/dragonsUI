776679ff2dbaea996eaf12ba271ecdd41416f895
Milene Vieira Lacerda <milenelacerda@icloud.com>
2019-06-20 15:59:12 -0300

    feat(app): iniciar app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing-module';
+import { DragonsModule } from './dragons/dragons.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    DragonsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
