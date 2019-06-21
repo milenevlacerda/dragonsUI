73b91d8112658cc6c733b6539c3da0c450f02c26
Milene Vieira Lacerda <milenelacerda@icloud.com>
2019-06-20 22:09:49 -0300

    feat(rotas): criar rotas de criar dragão

--- src/app/dragons/dragons-routing-module.ts
+++ src/app/dragons/dragons-routing-module.ts
@@ -2,11 +2,17 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 
 import { DragonsComponent } from './dragons.component';
+import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DragonsComponent,
+  },
+  {
+    path: 'cadastrar',
+    component: CreateComponent,
+    outlet: 'modal',
   }
 ];
 
